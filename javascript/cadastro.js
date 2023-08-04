384d4338a8a283663d1fbb4b4c850cd817314010
[Alexandra Carneiro] <[alexandracarneiro@live.com]>
2023-08-04 11:32:38 +0100

    Validações iniciais JS

--- javascript/cadastro.js
+++ javascript/cadastro.js
@@ -1,34 +1,34 @@
 function validarFormulario() {
-    var nome = document.getElementById("nome").value;
-    var email = document.getElementById("email").value;
-    var senha = document.getElementById("senha").value;
-    var confirmarSenha = document.getElementById("confirmar-senha").value;
+  var nome = document.getElementById("nome").value;
+  var email = document.getElementById("email").value;
+  var senha = document.getElementById("senha").value;
+  var confirmarSenha = document.getElementById("confirmar-senha").value;
 
-    if (nome === "") {
-      alert("Por favor, preencha o campo nome.");
-      return false;
-    }
-
-    if (email === "") {
-      alert("Por favor, preencha o campo email.");
-      return false;
-    }
-
-    if (senha === "") {
-      alert("Por favor, preencha o campo senha.");
-      return false;
-    }
-
-    if (confirmarSenha === "") {
-      alert("Por favor, preencha o campo confirmar senha.");
-      return false;
-    }
-
-    if (senha !== confirmarSenha) {
-      alert("As senhas não coincidem.");
-      return false;
-    }
-
-    alert("Formulário enviado com sucesso!");
-    return true;
+  if (nome.trim() === "") {
+    alert("Por favor, preencha o campo Nome.");
+    return false;
   }
+
+  if (email.trim() === "") {
+    alert("Por favor, preencha o campo Email.");
+    return false;
+  }
+
+  if (senha.trim() === "") {
+    alert("Por favor, preencha o campo Senha.");
+    return false;
+  }
+
+  if (confirmarSenha.trim() === "") {
+    alert("Por favor, preencha o campo Confirmar Senha.");
+    return false;
+  }
+
+  if (senha !== confirmarSenha) {
+    alert("As senhas não coincidem.");
+    return false;
+  }
+  
+  alert("Cadastro realizado com sucesso");
+  return true;
+}
